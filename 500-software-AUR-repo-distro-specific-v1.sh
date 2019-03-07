5ecc790a65d51fd0aef902fb49c26fddf9e4a79e
Opentux <tessier.julien@gmail.com>
2019-03-07 19:11:38 +0100

    add timeshift

--- 500-software-AUR-repo-distro-specific-v1.sh
+++ 500-software-AUR-repo-distro-specific-v1.sh
@@ -20,13 +20,14 @@ echo "AUR - DESKTOP SPECIFIC APPLICATIONS "
 
 sh AUR-DS/install-gnome-terminal-transparency-v*.sh
 
-sh AUR-DS/install-nautilus-compare-v*.sh
+#sh AUR-DS/install-nautilus-compare-v*.sh
 
 sh AUR-DS/install-nautilus-image-tools-v*.sh
 
 sh AUR-DS/install-nautilus-renamer-v*.sh
 
-sh AUT-DS/intel-undervolt-v*.sh 
+sh AUR-DS/install-intel-undervolt-v*.sh
+
 # And Enable service intel-undervolt
 sudo systemctl enable intel-undervolt
 
@@ -34,7 +35,9 @@ sh AUR-DS/install-appimagelauncher-v*.sh
 
 #sh AUR/install-yad-v*.sh
 
-sh AUR/install-nvidia-xrun-v*.sh
+sh AUR-DS/install-nvidia-xrun-v*.sh
+
+sh AUR-DS/install-timeshift-v*.sh
 
 echo "################################################################"
 echo "####       Software from AUR-DS folder installed          ######"
