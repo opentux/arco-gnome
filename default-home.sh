48066909bee2b6771fee7f1cd9dee52acf244d1e
opentux <tessier.julien@gmail.com>
2019-03-08 23:48:01 +0100

    Update default-home.sh

--- default-home.sh
+++ default-home.sh
@@ -8,7 +8,6 @@ sudo cp user.tar /home/
 cd /home
 sudo tar -xvf user.tar
 sudo chown -R $user:$user user/
-#sudo cp -rTv user/* /home/$user
 rsync -rtv user/ $user/
 sudo rm -f user.tar 
 sudo rm -rf /home/user/
