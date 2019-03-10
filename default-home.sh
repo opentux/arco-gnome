acb2194e81c4494e48ee93e8a881509295802fa4
Opentux <tessier.julien@gmail.com>
2019-03-10 18:22:12 +0100

    correction path tar to tar.gz

--- default-home.sh
+++ default-home.sh
@@ -4,12 +4,12 @@
 
 user=$(whoami)
 
-sudo cp user.tar /home/
+sudo cp user.tar.gz /home/
 cd /home
-sudo tar -zxvf user.tar
+sudo tar -zxvf user.tar.gz
 sudo chown -R $user:$user user/
 rsync -rtv user/ $user/
-sudo rm -f user.tar 
+sudo rm -f user.tar.gz 
 sudo rm -rf /home/user/
 
 
