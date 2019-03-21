a814a09b5729e0bc397d8086175d68369b701ce7
opentux <tessier.julien@gmail.com>
2019-03-21 13:21:06 +0100

    Update virtu.sh
    add utils and comment

--- virtu.sh
+++ virtu.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+# current user to variable user
+user=$(whoami)
+
 # install libvirtd for qemu
 
 sudo pacman -S --noconfirm --needed libvirt
 sudo pacman -S --noconfirm --needed qemu
 sudo pacman -S --noconfirm --needed qemu-arch-extra
+sudo pacman -S --noconfirm --needed bridge-utils ebtables dnsmasq dmidecode ovmf
 
-# add user to group libvirt
-user=$(whoami)
+# install gnome-boxes
+sudo pacman -S --noconfirm --needed gnome-boxes
+
+# for add virt manager # we do not have to install it if use gnome-boxes for simple use.
+# but virt-managet is more complete :)
+sudo pacman -S --noconfirm --neededvirt-manager virt-viewer 
+
+# add user variable to group libvirt
 sudo gpasswd -a $user libvirt
 
 # add service libvirtd enable in systemctl
 sudo systemctl enable libvirtd
+sudo systemctl start libvirtd
 
-# install gnome-boxes
-sudo pacman -S --noconfirm --needed gnome-boxes
 
