a3ad4abb0ebe32a735cc1d708834976f879c785a
opentux <tessier.julien@gmail.com>
2019-03-21 13:25:48 +0100

    Update virtu.sh

--- virtu.sh
+++ virtu.sh
@@ -17,11 +17,20 @@ sudo pacman -S --noconfirm --needed gnome-boxes
 # but virt-managet is more complete :)
 sudo pacman -S --noconfirm --neededvirt-manager virt-viewer 
 
-# add user variable to group libvirt
+# add user variable to group libvirt & kvm
 sudo gpasswd -a $user libvirt
+sudo gpasswd -a $user kvm
 
 # add service libvirtd enable in systemctl
 sudo systemctl enable libvirtd
 sudo systemctl start libvirtd
 
+echo -e "options kvm-intel nested=1" | sudo tee -a /etc/modprobe.d/kvm-intel.conf
 
+echo '
+nvram = [
+    "/usr/share/ovmf/x64/OVMF_CODE.fd:/usr/share/ovmf/x64/OVMF_VARS.fd"
+]' | sudo tee --append /etc/libvirt/qemu.conf
+
+echo "Run this code in the terminal to get your network up and running after reboot"
+echo "sudo virsh net-start default"
