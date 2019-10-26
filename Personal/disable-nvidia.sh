5ca4729194e2e820a481ad695b1bd0d92161e640
opentux <tessier.julien@gmail.com>
2019-10-26 16:37:13 +0200

    Update disable-nvidia.sh
    disable bbswitch install

--- Personal/disable-nvidia.sh
+++ Personal/disable-nvidia.sh
@@ -5,22 +5,22 @@ user=$(whoami)
 # Disable nouveau kernel module
 sudo bash -c "echo 'blacklist nouveau' > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
 sudo bash -c "echo 'options nouveau modeset=0' >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-sudo bash -c "echo 'blacklist nvidia' >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-sudo bash -c "echo 'blacklist nvidia-modeset' >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-sudo bash -c "echo 'blacklist nvidia-uvm' >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-sudo bash -c "echo 'blacklist ttm' >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-sudo bash -c "echo 'blacklist drm_kms_helper' >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-sudo bash -c "echo 'blacklist drm' >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+#sudo bash -c "echo 'blacklist nvidia' >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+#sudo bash -c "echo 'blacklist nvidia-modeset' >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+#sudo bash -c "echo 'blacklist nvidia-uvm' >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+#sudo bash -c "echo 'blacklist ttm' >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+#sudo bash -c "echo 'blacklist drm_kms_helper' >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+#sudo bash -c "echo 'blacklist drm' >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
 
 sudo mkinitcpio -p linux
 
 
 #disable nvidia gpu for use nvidia-xrun#
-sudo pacman -S --noconfirm --needed bbswitch 
+#sudo pacman -S --noconfirm --needed bbswitch 
 #sudo pacman -S --noconfirm --needed bbswitch-dkms
 
-sudo bash -c "echo 'bbswitch ' > /etc/modules-load.d/bbswitch.conf"
-sudo bash -c "echo 'options bbswitch load_state=0 unload_state=0' > /etc/modprobe.d/bbswitch.conf"
+#sudo bash -c "echo 'bbswitch ' > /etc/modules-load.d/bbswitch.conf"
+#sudo bash -c "echo 'options bbswitch load_state=0 unload_state=0' > /etc/modprobe.d/bbswitch.conf"
 
 # install intel media driver and utils#
 
