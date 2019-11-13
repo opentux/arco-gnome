830cdd701bc617c2a52f70cd3323531214ce3d7e
opentux <tessier.julien@gmail.com>
2019-11-13 11:07:30 +0100

    Update 300-software-AUR-repo-v1.sh

--- 300-software-AUR-repo-v1.sh
+++ 300-software-AUR-repo-v1.sh
@@ -67,13 +67,13 @@ sh AUR/install-downgrade-v*.sh
 sh AUR/install-inxi-v*.sh
 sh AUR/install-neofetch-v*.sh
 #sh AUR/install-numix-circle-icon-theme-git-v*.sh
-sh AUR/install-oxy-neon-v*.sh
+#sh AUR/install-oxy-neon-v*.sh
 sh AUR/install-pamac-aur-v*.sh
 #sh AUR/install-paper-icon-theme-git-v*.sh
 #sh AUR/install-papirus-icon-theme-git-v*.sh
 #sh AUR/install-sardi-icons-v*.sh
 #sh AUR/install-sardi-extra-icons-v*.sh
-sh AUR/install-screenkey-v*.sh
+#sh AUR/install-screenkey-v*.sh
 #sh AUR/install-surfn-icons-git-v*.sh
 sh AUR/install-ttf-font-awesome-v*.sh
 sh AUR/install-ttf-mac-fonts-v*.sh
